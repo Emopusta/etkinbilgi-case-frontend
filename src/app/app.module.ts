3ca7f36593b6b840b570dd5e8160a4373e691a1f
Emopusta <emreduman225@hotmail.com>
2023-10-10 16:36:13 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AboutUsComponent } from './components/components/about-us/about-us.component';
+import { ContactMeComponent } from './components/components/contact-me/contact-me.component';
+import { NavbarComponent } from './components/components/navbar/navbar.component';
+import { FooterComponent } from './components/components/footer/footer.component';
+import { HomeComponent } from './components/components/home/home.component';
+import { AuthInterceptor } from './interceptors/auth/auth.interceptor';
+import { LoginComponent } from './components/components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactMeComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi:true,
+  },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
