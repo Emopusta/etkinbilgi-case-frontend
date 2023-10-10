bd67b5b467d141ed49bdc789388a66e1172fb209
Emopusta <emreduman225@hotmail.com>
2023-10-10 16:35:49 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/components/home/home.component';
+import { AboutUsComponent } from './components/components/about-us/about-us.component';
+import { ContactMeComponent } from './components/components/contact-me/contact-me.component';
+import { LoginComponent } from './components/components/login/login.component';
+import { LoginGuard } from './guards/login/login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about-us', component: AboutUsComponent, canActivate: [LoginGuard] },
+  { path: 'contact', component: ContactMeComponent, canActivate: [LoginGuard] },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
