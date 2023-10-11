3cf864cbf69cbfdf4966b21d18f3437ec07f0a54
Emopusta <emreduman225@hotmail.com>
2023-10-11 23:41:47 +0300

    added add personnel page and its logic

--- src/app/components/personnel/add-personnel/add-personnel.component.ts
+++ src/app/components/personnel/add-personnel/add-personnel.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PersonnelService } from 'src/app/services/personnel/personnel.service';
 
 @Component({
   selector: 'app-add-personnel',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-personnel.component.css']
 })
 export class AddPersonnelComponent {
+  personnelForm:FormGroup;
+  selectedImage:File | null = null;
 
+  constructor(private formBuilder: FormBuilder,
+    private personnelService: PersonnelService){
+    this.personnelForm = this.formBuilder.group({
+      email : ['', Validators.required],
+    });
+  }
+  savePersonnel(){
+    const formData = new FormData();
+    formData.append('email', this.personnelForm.value.email);
+    formData.append('image', this.selectedImage??"");
+
+    this.personnelService.addPersonnel(formData).subscribe(response => {
+      console.log("response => ", response);
+    },
+    error => {
+      console.log("error => ", error.error);
+    });
+  }
+
+  onImageSelect(event:any){
+    this.selectedImage = event.target.files[0]
+  }
 }
