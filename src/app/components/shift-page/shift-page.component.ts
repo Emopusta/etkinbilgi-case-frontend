4a4119a7e18fa611773421a61d85ccb5bf5ae1c5
Emopusta <emreduman225@hotmail.com>
2023-10-12 11:35:49 +0300

    added shift page

--- src/app/components/shift-page/shift-page.component.ts
+++ src/app/components/shift-page/shift-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shift-page',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./shift-page.component.css']
 })
 export class ShiftPageComponent {
+  constructor(private router:Router){
 
+  }
+  getAdd(){
+    this.router.navigate(["shift-page/add-shift"]);
+  }
+  getList(){
+    this.router.navigate(["shift-page/list-shift"]);
+  }
 }
