e521a6ec1a5840520498123945372599a4ece3ea
Emopusta <emreduman225@hotmail.com>
2023-10-12 11:36:39 +0300

    added shift service

--- src/app/services/personnel-shift/personnel-shift.service.ts
+++ src/app/services/personnel-shift/personnel-shift.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddShiftModel } from 'src/app/models/add-shift-model';
+import { ListResponseModel } from 'src/app/models/list-response-model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,18 @@ export class PersonnelShiftService {
 
   apiUrl = "http://localhost:60805/api/PersonnelShifts/"
   constructor(private httpClient:HttpClient) { }
+
+  addShift(formData:FormData){
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    return this.httpClient.post<AddShiftModel>(this.apiUrl, formData, { headers });
+  }
+
+  getListShift(index:number, size:number){
+
+    return this.httpClient.get<ListResponseModel>(this.apiUrl+ "?PageIndex="+index+"&PageSize="+size);
+
+  }
 
   
 
